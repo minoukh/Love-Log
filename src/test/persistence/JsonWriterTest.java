ca4adef254b2a852594ba3376cd39440cc9aebd5
minoukh <minoukhadem@gmail.com>
2023-02-28 14:28:42 -0800

    Modified Phase+ persistence tests 2 Feb 28

--- src/test/persistence/JsonWriterTest.java
+++ src/test/persistence/JsonWriterTest.java
@@ -1,4 +1,67 @@
 package persistence;
 
-public class JsonWriterTest {
+import model.Person;
+import model.MyJournal;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class JsonWriterTest extends JsonTest {
+
+    @Test
+    void testWriterInvalidFile() {
+        try {
+            MyJournal myJournal = new MyJournal("My journal");
+            JsonWriter writer = new JsonWriter("./data/my\0illegal:fileName.json");
+            writer.open();
+            fail("IOException was expected");
+        } catch (IOException e) {
+            // pass
+        }
+    }
+
+    @Test
+    void testWriterEmptyJournal() {
+        try {
+            MyJournal myJournal = new MyJournal("My journal");
+            JsonWriter writer = new JsonWriter("./data/testWriterEmptyJournal.json");
+            writer.open();
+            writer.write(myJournal);
+            writer.close();
+
+            JsonReader reader = new JsonReader("./data/testWriterEmptyJournal.json");
+            myJournal = reader.read();
+            assertEquals("My journal", myJournal.getName());
+            assertEquals(0, myJournal.numPersonsDating());
+        } catch (IOException e) {
+            fail("Exception should not have been thrown");
+        }
+    }
+
+    @Test
+    void testWriterGeneralWorkroom() {
+        try {
+            MyJournal myJournal = new MyJournal("My journal");
+            myJournal.addPerson(new Person("Jill", 25, "Kits", "lawyer",10));
+            myJournal.addPerson(new Person("Jane", 35, "Burnaby", "Doctor",20));
+            JsonWriter writer = new JsonWriter("./data/testWriterGeneralJournal.json");
+            writer.open();
+            writer.write(myJournal);
+            writer.close();
+
+            JsonReader reader = new JsonReader("./data/testWriterGeneralJournal.json");
+            myJournal = reader.read();
+            assertEquals("My journal", myJournal.getName());
+            List<Person> personList = myJournal.getListOfPerson();
+            assertEquals(2, personList.size());
+            checkPerson("Jill", 25, "Kits", "lawyer", 10, personList.get(0));
+            checkPerson("Jane", 35, "Burnaby", "Doctor",20, personList.get(1));
+
+        } catch (IOException e) {
+            fail("Exception should not have been thrown");
+        }
+    }
 }
